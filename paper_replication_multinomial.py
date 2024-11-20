1b155690e31c7f07a859d4453fc1a71a0a9a925e
Jong-Min-Moon <jm.moon@yonsei.ac.kr>
2024-11-19 18:12:52 -0800

    fixed type

--- paper_replication_multinomial.py
+++ paper_replication_multinomial.py
@@ -60,14 +60,14 @@ for i in range(n_test):
         LDPclient.release_private(
             priv_mech,
             data_gen.generate_multinomial_data(p1, sample_size),
-            k,
+            d,
             privacy_level,
             device
         ),
         LDPclient.release_private(
             priv_mech,
             data_gen.generate_multinomial_data(p2, sample_size),
-            k,
+            d,
             privacy_level,
             device
         ),
@@ -80,8 +80,6 @@ for i in range(n_test):
 
     p_value_vec[i], statistic_vec[i] = server_private.release_p_value_permutation(n_permutation)
     t_end_i = time.time() - t_start_i
-    data_entry = (test_num, d, param_dist, privacy_level, sample_size, statistic, priv_mech, statistic_vec[i].item(), p_value_vec[i].item(), float(t_end_i), time_now, n_bin, n_permutation)
-    print(data_entry)
 
     print(f"pval: {p_value_vec[i]} -- {test_num}th test, time elapsed {t_end_i} -- emperical power so far (from test_start): {(p_value_vec[0:(i+1)] < significance_level).mean()}")
 
